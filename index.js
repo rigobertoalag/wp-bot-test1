da27e3782cacbe322ad4c60b2ce8ab0b903ba6f2
rigobertoalag <a20150338@utem.edu.mx>
2022-05-04 19:03:01 -0500

    Add previos functions and redesign message

--- index.js
+++ index.js
@@ -1,72 +1,90 @@
+const qrcode = require('qrcode-terminal')
+const axios = require('axios')
 
-const fs = require('fs');
-const { Client, LegacySessionAuth, LocalAuth, NoAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
-
-const { Pool } = require('pg')
-
-const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'wp_bot_1',
-    password: 'martin51',
-    port: 5432,
-})
-
-const tquery = 'select now()'
-
-const query = 'select cntr_contenedor, cntr_pedimento, cntr_fecha_ingreso, cntr_tipo, cntr_peso from containers where cntr_contenedor = $1'
-const values = ['TTSSTT001122']
-
-let resultado
-pool.query(query, values, (err, res) => {
-    console.log(res.rows[0])
-    test(JSON.stringify(res.rows[0]))
-    resultado = JSON.stringify(res.rows[0]);
-    console.log('desde resultado: ', resultado)
-    pool.end()
-})
-
-function test(data){
-    resultado = 'Testeo:' + data
-    console.log('ejecuta la funcion', resultado)
-}
-
-
-// Use the saved values
+const { Client, LocalAuth } = require('whatsapp-web.js')
 const client = new Client({
-    authStrategy: new NoAuth()
-});
+    authStrategy: new LocalAuth()
+})
 
 client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
-});
+    qrcode.generate(qr, { small: true })
+})
 
-client.on('message', message => {
-    if (message.body === 'Hola') {
-        let mssgToSendTT
-        pool.query(tquery, (err, res) => {
-            const resultadoTest = JSON.stringify(res.rows[0]);
-            mssgToSendTT = resultadoTest
-            pool.end()
-        })
-        console.log('llego el mensaje y mando los datos')
-        client.sendMessage(message.from, mssgToSendTT);
-    } else if (message.body) {
-        const values = [message.body]
-        pool.query(query, values, (err, res) => {
-            const resultado = res.rows[0]
-            client.sendMessage(message.from, resultado);
-            pool.end()
-        })
+client.on('ready', () => {
+    console.log('Client is ready')
+})
+
+client.on('message', async message => {
+    const content = message.body
+    const contentDiv = content.split(" ")
+
+    const contentOption = contentDiv[0].toLowerCase()
+    const contentSearch = contentDiv[1]
+
+    const welcomeOptions = [
+        'hola', 'buen día', 'buen dia','buenos días', 'buenas tardes','buena tarde'
+    ]
+
+    if (contentOption === welcomeOptions) {
+       const msgToSendWelcome = `Buen día, para consultar informacion necesaria solo escribre el nombre del dato que deseas y su identificador \n Ejemplo: \n Contenedor *YMLU5145696* \n Opciones disponibles: \nContenedor *busca informacion del contenedor* \nPrevio *Datos sobre los previos*`
+
+        client.sendMessage(message.from, msgToSendWelcome);
+    } else if (contentOption === 'contenedor') {
+
+        const contenedor = await axios('http://localhost:8000/api/containers/2')
+            .then(res => res.data)
+
+        const msgToSend = `🚛 El contenedor es: ${contenedor.cntr_contenedor}\n🗺️Es de tipo: ${contenedor.cntr_tipo}\n📏Tamaño: ${contenedor.cntr_tamano}\n📝Pedimento: ${contenedor.cntr_pedimento}\n⬇️ Arribó el día: ${contenedor.cntr_fecha_arribo}`
+
+
+        client.sendMessage(message.from, msgToSend)
+        console.log('entro a la opcion contenedor')
+    } else if (contentOption === 'previo') {
+        const request = await axios('http://localhost:8000/api/previos/' + contentSearch)
+            .then(res => res.data)
+
+        const msgToSendCntr = `📑 Numero de previo: ${request[0].prev_consecutivo}\n🧱 Cantidad: ${request[0].prev_cantidad}\n⚖️ Peso: ${request[0].prev_peso}\n📍 Origen: ${request[0].prev_origen}`
+        client.sendMessage(message.from, msgToSendCntr)
+        console.log('entro a la opcion de previo')
     }
-    // if (message.body === '!ping') {
-    //     message.reply('pong');
-    //     console.log('Llego el mensaje')
-    // } else if (message.body === 'Hola') {
-    //     message.sendMessage('¿Como estas?');
-    //     console.log('Llego el mensaje')
-    // }
-});
+})
 
-client.initialize();
+const apiTest = async () => {
+    //constante que simula la opcion que eligio el user|
+    const content = 'previo ABJ2000002'
+
+    //Se realiza el split de la palabra clave y el dato a buscar
+    const contentDiv = content.split(" ")
+    //Opcion elegida
+    const contentOption = contentDiv[0].toLowerCase()
+    //  //Se convierte a minusculas 
+    // const contentConvert = contentDiv[0].toLowerCase()
+    //Dato para buscar en la bd
+    const contentSearch = contentDiv[1]
+
+    console.log(contentSearch)
+
+    // const msgToSendPrev = `El consecutivo es: ${}`
+
+    if (contentOption === 'contenedor') {
+        const request = await axios('http://localhost:8000/api/containers/' + contentSearch)
+            .then(res => res.data)
+
+        const msgToSendCntr = `🚛 El contenedor es: ${request[0].cntr_contenedor}\n🗺️ Es de tipo: ${request[0].cntr_tipo}\n📏Tamaño: ${request[0].cntr_tamano}\n📝Pedimento: ${request[0].cntr_pedimento}\n⬇️ Arribó el día: ${request[0].cntr_fecha_arribo}`
+        console.log(msgToSendCntr)
+    } else if (contentOption === 'previo') {
+        const request = await axios('http://localhost:8000/api/previos/' + contentSearch)
+            .then(res => res.data)
+
+        const msgToSendCntr = `📑 Numero de previo: ${request[0].prev_consecutivo}\n🧱 Cantidad: ${request[0].prev_cantidad}\n⚖️ Peso: ${request[0].prev_peso}\n📍 Origen: ${request[0].prev_origen}`
+        console.log(msgToSendCntr)
+        console.log(request)
+    } else {
+        console.log('No hay info')
+    }
+
+}
+
+apiTest()
+
+client.initialize()
